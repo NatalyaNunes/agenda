e5416275556e77365893fc9583c94e44824c5229
Natálya Gabriele Nunes de Azevedo <nunes.natalya@academico.ifrn.edu.br>
2022-12-23 11:28:34 -0300

    atualizacao

--- database/migrations/2022_12_23_134930_create_usuarios_table.php
+++ database/migrations/2022_12_23_134930_create_usuarios_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();            
+            $table->string('nome' , 100);
+            $table->string('endereco', 300);
+            $table->string('senha', 150);
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usuarios');
     }
 };
--- database/migrations/2022_12_23_135736_create_contatos_table.php
+++ database/migrations/2022_12_23_135736_create_contatos_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('contatos', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->foreignId('usuarios')->constrained(); /* para criar chave estrangeira */
+            $table->string('nome', 100);
+            $table->integer('ddd');
+            $table->string('telefone', 9);
+            $table->timestamps();
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('contatos');
     }
 };
